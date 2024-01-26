baa4c2fda0654863bed6a97dc178b4918cea9f4b
chiara.ruggi <chiara.ruggi1998@gmail.com>
2024-01-26 11:45:38 +0100

    header in app.data

--- config/data.php
+++ config/data.php
@@ -1,5 +1,57 @@
 <?php
 
 return [
-    "home" => 42
+    "menuLinks" => [
+        [
+            "text" => "Characters",
+            "url" => "#characters",
+
+        ],
+        
+        [
+                "text" => "Comics",
+                "url" => "#comics",
+    
+        ],
+        [
+            "text" => "Movies",
+            "url" => "#movies",
+
+        ],
+        [
+            "text" => "Tv",
+            "url" => "#tv",
+
+        ],
+        [
+            "text" => "Games",
+            "url" => "#games",
+
+        ],
+        [
+            "text" => "Collectibles",
+            "url" => "#collectibles",
+
+        ],
+        [
+            "text" => "Videos",
+            "url" => "#movies",
+
+        ],
+        [
+            "text" => "Fans",
+            "url" => "#fans",
+
+        ],
+        [
+            "text" => "News",
+            "url" => "#news",
+
+        ],
+        [
+            "text" => "Shop",
+            "url" => "#shop",
+
+        ],
+      ]
 ];
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,29 @@
 <header>
-    <a href="{{ route('home') }}">Home</a>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="d-flex justify-content-between align-items-center">
+                    <div class="my-logo-container my-3">
+                        <img src="/img/dc-logo.png" alt="">
+                    </div>
+                    <div class="my-menu-container">
+                        @foreach ($menuLinks as $link)
+                            <a href="#" class="mx-3">{{ link . text }}</a>
+                        @endforeach
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </header>
+
+<style>
+    .my-logo-container img {
+        width: 80%;
+    }
+
+    a {
+        text-decoration: none;
+        color: black;
+    }
+</style>
